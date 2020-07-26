f7bdb9bb6cf5e80be8a192ace0f6a57c0750cd73
Pierre Chalamet <pierre@chalamet.net>
2020-07-26 21:56:33 +0200

    handle all errors in message callback

--- fbus/Bus.fs
+++ fbus/Bus.fs
@@ -24,36 +24,36 @@ type Bus(busBuilder: BusBuilder) =
         | Some busTransport -> busBuilder.Serializer.Serialize msg |> busTransport.Send headers client (msg |> getMsgType)
 
     let msgCallback activationContext headers msgType content =
-        let conversationHeaders () = 
-            defaultHeaders |> Map.add "fbus:message-id" (Guid.NewGuid().ToString())
-                           |> Map.add "fbus:conversation-id" (headers |> Map.find "fbus:conversation-id")
-
-        let ctx = { new IBusConversation with
-                        member _.ConversationId: string = headers |> Map.find "fbus:conversation-id"
-                        member _.MessageId: string = headers |> Map.find "fbus:message-id"
-                        member _.Sender: string = headers |> Map.find "fbus:sender"
-                        member this.Reply msg = conversationHeaders() |> send this.Sender msg
-                        member _.Publish msg = conversationHeaders() |> publish msg
-                        member _.Send client msg = conversationHeaders() |> send client msg }
-
-        let mutable msg: obj = null
         try
-            let handlerInfo = match busBuilder.Handlers |> Map.tryFind msgType with
-                              | Some handlerInfo -> handlerInfo
-                              | _ -> failwithf "Unknown message type [%s]" msgType
-            let handler = busBuilder.Container.Resolve activationContext handlerInfo
-            if handler |> isNull then failwith "No handler found"
+            let conversationHeaders () = 
+                defaultHeaders |> Map.add "fbus:message-id" (Guid.NewGuid().ToString())
+                               |> Map.add "fbus:conversation-id" (headers |> Map.find "fbus:conversation-id")
 
-            let callsite = handlerInfo.InterfaceType.GetMethod("Handle")
-            if callsite |> isNull then failwith "Handler method not found"
+            let ctx = { new IBusConversation with
+                            member _.ConversationId: string = headers |> Map.find "fbus:conversation-id"
+                            member _.MessageId: string = headers |> Map.find "fbus:message-id"
+                            member _.Sender: string = headers |> Map.find "fbus:sender"
+                            member this.Reply msg = conversationHeaders() |> send this.Sender msg
+                            member _.Publish msg = conversationHeaders() |> publish msg
+                            member _.Send client msg = conversationHeaders() |> send client msg }
 
-            msg <- busBuilder.Serializer.Deserialize handlerInfo.MessageType content
-            callsite.Invoke(handler, [| ctx; msg |]) |> ignore
+            let mutable msg: obj = null
+            try
+                let handlerInfo = match busBuilder.Handlers |> Map.tryFind msgType with
+                                  | Some handlerInfo -> handlerInfo
+                                  | _ -> failwithf "Unknown message type [%s]" msgType
+                let handler = busBuilder.Container.Resolve activationContext handlerInfo
+                if handler |> isNull then failwith "No handler found"
+
+                let callsite = handlerInfo.InterfaceType.GetMethod("Handle")
+                if callsite |> isNull then failwith "Handler method not found"
+
+                msg <- busBuilder.Serializer.Deserialize handlerInfo.MessageType content
+                callsite.Invoke(handler, [| ctx; msg |]) |> ignore
+            with
+                exn -> busBuilder.Hook |> Option.iter (fun hook -> hook.OnError ctx msg exn)
         with
-            exn -> try
-                       busBuilder.Hook |> Option.iter (fun hook -> hook.OnError ctx msg exn)
-                   with
-                       exn -> printfn "Failure in error handler %A" exn
+            exn -> printfn "General failure in message callback %A" exn
 
     let newConversationHeaders () =
         defaultHeaders |> Map.add "fbus:conversation-id" (Guid.NewGuid().ToString())
