367807f27ad08d10459e392432bdc945621b01ab
Pierre Chalamet <pierre@chalamet.net>
2020-11-07 12:01:37 +0100

    avoid raising exception twice

--- FBus/Control.fs
+++ FBus/Control.fs
@@ -25,40 +25,37 @@ type Bus(busConfig: BusConfiguration) =
         | Some busTransport -> busConfig.Serializer.Serialize msg |> busTransport.Send headers client (msg |> getMsgType)
 
     let msgCallback activationContext headers msgType content =
-        try
+        let mutable msg: obj = null
+        let ctx = 
             let conversationHeaders () = 
                 defaultHeaders |> Map.add "fbus:message-id" (Guid.NewGuid().ToString())
                                |> Map.add "fbus:conversation-id" (headers |> Map.find "fbus:conversation-id")
 
-            let ctx = { new IBusConversation with
-                            member _.ConversationId: string = headers |> Map.find "fbus:conversation-id"
-                            member _.MessageId: string = headers |> Map.find "fbus:message-id"
-                            member _.Sender: string = headers |> Map.find "fbus:sender"
-                            member this.Reply msg = conversationHeaders() |> send this.Sender msg
-                            member _.Publish msg = conversationHeaders() |> publish msg
-                            member _.Send client msg = conversationHeaders() |> send client msg }
+            { new IBusConversation with
+                    member _.ConversationId: string = headers |> Map.find "fbus:conversation-id"
+                    member _.MessageId: string = headers |> Map.find "fbus:message-id"
+                    member _.Sender: string = headers |> Map.find "fbus:sender"
+                    member this.Reply msg = conversationHeaders() |> send this.Sender msg
+                    member _.Publish msg = conversationHeaders() |> publish msg
+                    member _.Send client msg = conversationHeaders() |> send client msg }
 
-            let mutable msg: obj = null
-            try
-                let handlerInfo = match busConfig.Handlers |> Map.tryFind msgType with
-                                  | Some handlerInfo -> handlerInfo
-                                  | _ -> failwithf "Unknown message type [%s]" msgType
-                let handler = busConfig.Container.Resolve activationContext handlerInfo
-                if handler |> isNull then failwith "No handler found"
+        try
+            let handlerInfo = match busConfig.Handlers |> Map.tryFind msgType with
+                              | Some handlerInfo -> handlerInfo
+                              | _ -> failwithf "Unknown message type [%s]" msgType
+            let handler = busConfig.Container.Resolve activationContext handlerInfo
+            if handler |> isNull then failwith "No handler found"
 
-                let callsite = handlerInfo.InterfaceType.GetMethod("Handle")
-                if callsite |> isNull then failwith "Handler method not found"
+            let callsite = handlerInfo.InterfaceType.GetMethod("Handle")
+            if callsite |> isNull then failwith "Handler method not found"
 
-                msg <- busConfig.Serializer.Deserialize handlerInfo.MessageType content
-                callsite.Invoke(handler, [| ctx; msg |]) |> ignore
-            with
-                | :? Reflection.TargetInvocationException as tie -> busConfig.Hook |> Option.iter (fun hook -> hook.OnError ctx msg tie.InnerException)
-                                                                    reraise()
-                | exn -> busConfig.Hook |> Option.iter (fun hook -> hook.OnError ctx msg exn)
-                         reraise()
+            msg <- busConfig.Serializer.Deserialize handlerInfo.MessageType content
+            callsite.Invoke(handler, [| ctx; msg |]) |> ignore
         with
-            exn -> printfn "General failure in message callback %A" exn
-                   reraise()
+            | :? Reflection.TargetInvocationException as tie -> busConfig.Hook |> Option.iter (fun hook -> hook.OnError ctx msg tie.InnerException)
+                                                                reraise()
+            | exn -> busConfig.Hook |> Option.iter (fun hook -> hook.OnError ctx msg exn)
+                     reraise()
 
     let newConversationHeaders () =
         defaultHeaders |> Map.add "fbus:conversation-id" (Guid.NewGuid().ToString())
