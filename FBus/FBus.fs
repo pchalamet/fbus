3c67708f0380cb63d612f311efced7e4df1ee7d8
Pierre Chalamet <pierre@chalamet.net>
2021-05-15 10:01:29 +0200

    param name on sig

--- FBus/FBus.fs
+++ FBus/FBus.fs
@@ -38,8 +38,8 @@ type IBusContainer =
 
 type IBusTransport =
     inherit IDisposable
-    abstract Publish: headers:Map<string, string> -> string -> body:ReadOnlyMemory<byte> -> unit
-    abstract Send: headers:Map<string, string> -> string -> target:string -> body:ReadOnlyMemory<byte> -> unit
+    abstract Publish: headers:Map<string, string> -> msgtype:string -> body:ReadOnlyMemory<byte> -> unit
+    abstract Send: headers:Map<string, string> -> msgtype:string -> target:string -> body:ReadOnlyMemory<byte> -> unit
 
 type IBusSerializer =
     abstract Serialize: msg:obj -> ReadOnlyMemory<byte>
